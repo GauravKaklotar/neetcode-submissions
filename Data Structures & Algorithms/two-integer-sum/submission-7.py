f5ca5aa21ff120cc458ae83c511b3ebdd8674463
Gaurav Kaklotar <gaurav.kaklotar03@gmail.com>
2026-04-02 00:31:09 +0530

    Add: two-integer-sum - submission-7

---
+++ Data Structures & Algorithms/two-integer-sum/submission-7.py
@@ -0,0 +1,28 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        n = len(nums)
+
+        d = dict()
+
+        for i, num in enumerate(nums):
+            if num in d:
+                prev_ind, count = d[num]
+                d[num] = (i, count+1)
+            else:
+                d[num] = (i, 1)
+
+        for i, num in enumerate(nums):
+            val = target - num
+            if val in d:
+                index, count = d[val]
+                if val == num:
+                    if count > 1:
+                        return [i, index]
+                    else:
+                        continue
+                
+                return [i, index]
+
+        # Time : O(n)
+        # Space: O(n)
+                    
