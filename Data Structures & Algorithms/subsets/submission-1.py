8c6e8548b6455f942a4aefa0194d0daa2f78759d
Gaurav Kaklotar <gaurav.kaklotar03@gmail.com>
2026-06-30 22:30:37 +0530

    Add: subsets - submission-1

---
+++ Data Structures & Algorithms/subsets/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+        n = len(nums)
+
+        def solve(i, curr):
+            nonlocal ans
+            if i == n:
+                ans.append(curr[:])
+                return
+            
+            curr.append(nums[i])
+            
+            solve(i+1, curr)
+
+            curr.pop()
+
+            solve(i+1, curr)
+        
+        solve(0, [])
+
+        return ans
