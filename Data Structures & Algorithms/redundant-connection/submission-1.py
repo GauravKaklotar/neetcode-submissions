a4084aa3df4eca84ec213864e02b51ca78acb0a7
Gaurav Kaklotar <gaurav.kaklotar03@gmail.com>
2026-07-24 01:54:10 +0530

    Add: redundant-connection - submission-1

---
+++ Data Structures & Algorithms/redundant-connection/submission-1.py
@@ -0,0 +1,66 @@
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        # 2 Solutions 
+        # Union Find
+        n = len(edges)
+
+        parent = [i for i in range(n+1)]
+        rank = [1] * (n+1)
+
+        def find(x):
+            if parent[x] != x:
+                parent[x] = find(parent[x])
+            
+            return parent[x]
+        
+        def union(x, y):
+            rootX, rootY = find(x), find(y)
+
+            # Cycle detected
+            if rootX == rootY:
+                return False
+            
+            if rank[rootX] > rank[rootY]:
+                parent[rootY] = rootX
+            elif rank[rootX] < rank[rootY]:
+                parent[rootX] = rootY
+            else:
+                parent[rootY] = rootX
+                rank[rootX] += 1
+            
+            return True
+
+        for u, v in edges:
+            if not union(u, v):
+                return [u, v]
+
+            
+        # ------------ DFS ------------------
+
+        # from collections import defaultdict
+
+        # graph = defaultdict(list)
+
+        # # Logic: Before adding edge we'll check if there's a way to reach from u --> v then return that [u, v] as it'll create cycle
+
+        # def dfs(src, target, visited):
+        #     if src == target:
+        #         return True
+            
+        #     visited.add(src)
+
+        #     for nei in graph[src]:
+        #         if nei not in visited:
+        #             if dfs(nei, target, visited):
+        #                 return True
+
+        #     return False
+        
+
+        # for u, v in edges:
+        #     if u in graph and v in graph:
+        #         if dfs(u, v, set()):
+        #             return [u, v]
+
+        #     graph[u].append(v)
+        #     graph[v].append(u)
