ac36208fb571d4dacb9e01a5fcb9d69bc3b1ae6c
Gaurav Kaklotar <gaurav.kaklotar03@gmail.com>
2026-07-28 20:14:37 +0530

    Add: min-cost-climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        
+        n = len(cost)
+
+        from functools import lru_cache
+
+        @lru_cache(None)
+        def solve(i):
+
+            if i >= n:
+                return 0
+
+            return cost[i] + min(
+                solve(i + 1),
+                solve(i + 2)
+            )
+
+        return min(solve(0), solve(1))
