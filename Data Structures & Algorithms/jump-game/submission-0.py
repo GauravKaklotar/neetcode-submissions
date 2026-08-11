b6f2ea862f5f56a0668aef9899d36f7d022a36b8
Gaurav Kaklotar <gaurav.kaklotar03@gmail.com>
2026-08-11 20:42:08 +0530

    Add: jump-game - submission-0

---
+++ Data Structures & Algorithms/jump-game/submission-0.py
@@ -0,0 +1,19 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        
+        n = len(nums)
+
+        from functools import lru_cache
+
+        @lru_cache(None)
+        def solve(i):
+            if i>=n-1:
+                return True
+            
+            for jump in range(1, nums[i] + 1):
+                if solve(i + jump):
+                    return True
+
+            return False
+        
+        return solve(0)
