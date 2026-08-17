208cddeec9142c029104e42b24e0179e909be035
Gaurav Kaklotar <gaurav.kaklotar03@gmail.com>
2026-08-17 23:44:37 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -0,0 +1,62 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = []
+
+        res = [(''.join(sorted(str)), str) for str in strs]
+        print(res)
+
+        d = dict()
+
+        for sorted_s, s in res:
+            if sorted_s in d:
+                d[sorted_s].append(s)
+            else:
+                d[sorted_s] = [s]
+        
+        return list(d.values())
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # n = len(strs)
+
+        # from collections import defaultdict
+        # d = defaultdict(list)
+
+        # for str in strs:
+        #     freq = [0] * 26
+
+        #     for c in str:
+        #         freq[ord(c) - ord('a')] += 1
+            
+        #     d[tuple(freq)].append(str)
+        
+        # return list(d.values())
+
+        # Time: O(N)
+        # Space: O(N)
